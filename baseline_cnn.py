0ddfd578637611475b965cb2ba7362b9b8edd679
lucas-erlacher <lucaserlacher@gmail.com>
2023-12-07 15:12:00 +0100

    bright-lion-155

--- baseline_cnn.py
+++ baseline_cnn.py
@@ -1,52 +1,47 @@
-from pytorch_lightning.utilities.types import EVAL_DATALOADERS
 import torch
 import torch.nn as nn
 import yaml
 from loader import Loader
 import pytorch_lightning as pl
 from torch.optim import Adam
-import wandb
 from pytorch_lightning.loggers import WandbLogger
-from train_utils import to_starting_point, to_learning_space, from_learning_space
-import global_objects
-import numpy as np
-import matplotlib.pyplot as plt
+from utils_train import to_starting_point, to_learning_space, quick_eval
 
 config = yaml.safe_load(open("config.yaml", "r"))
 
-# training params
+################  PARAMETERS ################
+
+# training
 batch_size = 4
 lr = 0.0001
 max_epochs = 10
+eval_freq = 500
+# model
+kernel_size = 5
+pred_diff = True  # switch betwwen predicting the full spec or the diff to the target spec
 
-# wandb histogram plotting parameters
-bins = 200
-alpha = 0.5
+################  LOADERS  ################
+loader = Loader()
+train_dataloader = loader.get_train_loader(batch_size)
+test_dataloader = loader.get_test_loader(batch_size)
 
 ################  MODEL ################
 
-pred_diff = True  # switch betwwen predicting the full spec or the diff to the target spec
-
 class Baseline_CNN(pl.LightningModule):
     def __init__(self):
-        super().__init__()
-        # HYPERPARAMETERS
-        self.kernel_size = 5
-        d = 1
-        self.save_hyperparameters()  # I think this is necessary to save the hyperparameters to wandb
-        # MODEL
+        super().__init__()        
         self.act_func = nn.ReLU()
-        self.conv1 = nn.Conv2d(kernel_size=self.kernel_size, in_channels=1, out_channels=8, padding=2) 
+        self.conv1 = nn.Conv2d(kernel_size=kernel_size, in_channels=1, out_channels=8, padding=2) 
         self.bn1 = nn.BatchNorm2d(8)
-        self.conv2 = nn.Conv2d(kernel_size=self.kernel_size, in_channels=8, out_channels=16, padding=2)
+        self.conv2 = nn.Conv2d(kernel_size=kernel_size, in_channels=8, out_channels=16, padding=2)
         self.bn2 = nn.BatchNorm2d(16)
-        self.conv3 = nn.Conv2d(kernel_size=self.kernel_size, in_channels=16, out_channels=32, padding=2)
+        self.conv3 = nn.Conv2d(kernel_size=kernel_size, in_channels=16, out_channels=32, padding=2)
         self.bn3 = nn.BatchNorm2d(32)
-        self.conv4 = nn.Conv2d(kernel_size=self.kernel_size, in_channels=32, out_channels=16, padding=2)
+        self.conv4 = nn.Conv2d(kernel_size=kernel_size, in_channels=32, out_channels=16, padding=2)
         self.bn4 = nn.BatchNorm2d(16)
-        self.conv5 = nn.Conv2d(kernel_size=self.kernel_size, in_channels=16, out_channels=8, padding=2)
+        self.conv5 = nn.Conv2d(kernel_size=kernel_size, in_channels=16, out_channels=8, padding=2)
         self.bn5 = nn.BatchNorm2d(8)
-        self.conv6 = nn.Conv2d(kernel_size=self.kernel_size, in_channels=8, out_channels=1, padding=2)
+        self.conv6 = nn.Conv2d(kernel_size=kernel_size, in_channels=8, out_channels=1, padding=2)
 
     def forward(self, x):
         init = x  # save input for later use (in case we want to predict the diff)
@@ -64,14 +59,6 @@ class Baseline_CNN(pl.LightningModule):
         x = torch.sigmoid(x) # maybe no act funct at end ... but some transforms require output to be in [0,1] and I am not sure clipping is good for gradients
 
         return x
-    
-    def train_dataloader(self):
-        loader = Loader()
-        return loader.get_train_loader(batch_size)
-    
-    def test_dataloader(self):
-        loader = Loader()
-        return loader.get_test_loader(batch_size)
     
     # does all the computations that can be reused for training and validation
     def prepare_call_loss(self, batch):
@@ -103,77 +90,18 @@ class Baseline_CNN(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         _, _, _, loss = self.prepare_call_loss(batch)
         self.log("train_loss", loss)
-        if batch_idx != 0 and batch_idx % 500 == 0:
-            self.quick_eval()
+        if batch_idx != 0 and batch_idx % eval_freq == 0:
+            quick_eval(batch_size, self.prepare_call_loss, test_dataloader, self)
         return {"loss": loss}
-    
-    def quick_eval(self):  # TODO: the differences (image and histogram) are really more interesting over the entire test set and not just one example
-        x = None
-        y_hat = None
-        y = None
-        for batch_idx, batch in enumerate(self.test_dataloader()):
-            losses = list()
-            x, y, y_hat, loss = self.prepare_call_loss(batch)
-            losses.append(loss.detach().cpu().numpy())
-            loss = np.mean(losses) / self.batch_size
-            self.log("eval_loss", loss)
-            if batch_idx == 50:  # don't go over the entire test set (the full test set meant for the final evaluation)
-                break
-        # log images of one example to wandb
-        wandb.log({"specs": [wandb.Image(x[0], caption="input"), 
-                              wandb.Image(y_hat[0], caption="output"), 
-                              wandb.Image(y[0], caption="target")]})
-        fig, ax = plt.subplots()
-        plt.colorbar(ax.imshow(y[0].detach().cpu().numpy() - x[0].detach().cpu().numpy(), cmap="coolwarm"), ax=ax)
-        wandb.log({"changes needed to get from starting point to target": wandb.Image(plt, caption="diffs")})
-        plt.close(fig)
-        fig, ax = plt.subplots()
-        plt.colorbar(ax.imshow(y[0].detach().cpu().numpy() - y_hat[0].detach().cpu().numpy(), cmap="coolwarm"), ax=ax)
-        wandb.log({"changes needed to get from output to target": wandb.Image(plt, caption="diffs")})
-        plt.close(fig)
-        # create histograms as images and log them to wandb 
-        fig, ax = plt.subplots()
-        ax.hist(x[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="input")
-        ax.hist(y_hat[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="output")
-        ax.hist(y[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="target")
-        ax.legend()
-        wandb.log({"specs_histograms": wandb.Image(fig, caption="h")})
-        plt.close(fig) 
-        # clip the hists because they sometimes have peaks that make the rest of the hist hard to see
-        fig, ax = plt.subplots()
-        ax.hist(x[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="input")
-        ax.set_ylim(0, 15000)
-        ax.hist(y_hat[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="output")
-        ax.set_ylim(0, 15000)
-        ax.hist(y[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="target")
-        ax.set_ylim(0, 15000)
-        ax.legend()
-        wandb.log({"specs_histogram_clipped": wandb.Image(fig, caption="h_clipped")})
-        plt.close(fig)
-        # hists of diffs
-        fig, ax = plt.subplots()
-        ax.hist(y[0].detach().cpu().numpy().flatten() - x[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="changes needed to get from starting point to target")
-        ax.hist(y[0].detach().cpu().numpy().flatten() - y_hat[0].detach().cpu().numpy().flatten(), bins=bins, alpha=alpha, label="changes needed to get from output to target")
-        ax.legend()
-        wandb.log({"diffs_histograms": wandb.Image(fig, caption="h2")})   
-        plt.close(fig)
-        # log audio to wandb
-        x = from_learning_space(x[0].detach().cpu().numpy())
-        y_hat = from_learning_space(y_hat[0].detach().cpu().numpy())
-        y = from_learning_space(y[0].detach().cpu().numpy())
-        wandb.log({"audio": [wandb.Audio(global_objects.stft_system.invert_spectrogram(x), caption="input", sample_rate=config["sampling_rate"]), 
-                             wandb.Audio(global_objects.stft_system.invert_spectrogram(y_hat), caption="output", sample_rate=config["sampling_rate"]), 
-                             wandb.Audio(global_objects.stft_system.invert_spectrogram(y), caption="target", sample_rate=config["sampling_rate"])]})
 
     def configure_optimizers(self):
         optimizer = Adam(self.parameters())
         return optimizer
 
+################  TRAINING ################
 
 if __name__ == '__main__':
-    # run training
-    wandb_logger = WandbLogger(project='generative inversion')
-    wandb_logger.experiment.batch_size = batch_size
     model = Baseline_CNN()
+    wandb_logger = WandbLogger(project='generative inversion')
     trainer = pl.Trainer(max_epochs=max_epochs, logger=wandb_logger)
-    trainer.fit(model=model, train_dataloaders=model.train_dataloader())
+    trainer.fit(model=model, train_dataloaders=train_dataloader)
--- unet.py
+++ unet.py
@@ -1,28 +1,74 @@
 # this file has been taken from the tqdne repo (and then modified when needed)
 
 import torch
-from torch.nn import functional as F
-from typing import List
 from diffusers import get_cosine_schedule_with_warmup
 import pytorch_lightning as pl
 from diffusers import DDPMScheduler
 from tqdne_diffusers import DDPMPipeline1DCond
-from tqdne_diffusers import to_inputs
 from torch import nn
 from diffusers import UNet1DModel
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
 from pytorch_lightning.loggers import WandbLogger
 from loader import Loader
 import yaml
-from train_utils import to_starting_point, to_learning_space, from_learning_space
+from utils_train import to_starting_point, to_learning_space, quick_eval
 
 config = yaml.safe_load(open("config.yaml", "r"))
 
-# TODOs: 
+# TODOs:
 # get the cnn to learn someting useful again (now with the reduced image size)
+# get unet to learn something useful
 
-################    DIFFUSION WRAPPER    ################
+################  PARAMETERS ################
+#training
+eval_freq = 500
+batch_size = 1
+max_epochs = 10
+loader = Loader()
+train_dataloader = loader.get_train_loader(batch_size)
+test_dataloader = loader.get_test_loader(batch_size)
+optimizer_params = {
+    "learning_rate": 1e-4,
+    "lr_warmup_steps": 500,
+    "n_train": len(train_dataloader) // batch_size,
+    "seed": 0,
+    "batch_size": batch_size,
+    "max_epochs": max_epochs,
+}
+trainer_params = {
+    "accumulate_grad_batches": 1,
+    "gradient_clip_val": 1,
+    "precision": "32-true",  
+    "max_epochs": max_epochs,
+    "accelerator": "auto",
+    "devices": "auto",
+    "num_nodes": 1}
+# model
+prediction_type = "sample"
+channels = config["stft_num_channels"]
+timestep = 1 
+unet_params = {
+    "sample_size":timestep,
+    "in_channels":1,
+    "out_channels":1,
+    "block_out_channels":  (32, 64, 128),
+    "down_block_types": ('DownBlock1D', 'DownBlock1D', 'AttnDownBlock1D'),
+    "up_block_types": ('AttnUpBlock1D', 'UpBlock1D', 'UpBlock1D'),
+    "mid_block_type": 'UNetMidBlock1D',
+    "out_block_type": "OutConv1DBlock",
+    "extra_in_channels" : 0,
+    "act_fn": "swish",  # if I don't pick an act fn I get error ...
+}
+scheduler_params = {
+    "beta_schedule": "linear",
+    "beta_start": 0.0001,
+    "beta_end": 0.02,
+    "num_train_timesteps": 1000,
+    "prediction_type": prediction_type,
+    "clip_sample": False,
+}
+
+################    MODEL   ################
+
 class LightningDDMP(pl.LightningModule):
     def __init__(
         self,
@@ -42,22 +88,25 @@ class LightningDDMP(pl.LightningModule):
         self.pipeline = DDPMPipeline1DCond(self.net, self.noise_scheduler)
         self.save_hyperparameters()
 
-    def evaluate(self, low_res):
-        # Sample some signal from random noise (this is the backward diffusion process).
-        sig = self.pipeline(
-            low_res=low_res,
-            generator=torch.manual_seed(self.optimizer_params["seed"]),
-        ).audios
+    def prepare_call_loss(self, batch):
+        x, y = batch[0], batch[1]
 
-        return sig
+        x = x.to('cpu')
+        y = y.to('cpu')
+        x = to_starting_point(x)
+        x, y = to_learning_space(x, y)
+        x = x.to('cuda')
+        y = y.to('cuda')
 
-    def log_value(self, value, name, train=True, prog_bar=True):
-        if train:
-            self.log(f"train_{name}", value, prog_bar=prog_bar)
-        else:
-            self.log(f"val_{name}", value, prog_bar=prog_bar)
+        batch_size = x.shape[0]
+        second_dim = x.shape[2]
 
-    def forward_step(self, batch: list, timestep: int):
+        # reshape input to the format that the huggingface lib expects
+        x = x.flatten(1).float()
+        y = y.flatten(1).float()
+
+        x.unsqueeze_(1)  # add channel dimension
+
         # Sample a random timestep for each signal
         timesteps = torch.randint(
             0,
@@ -65,40 +114,28 @@ class LightningDDMP(pl.LightningModule):
             (batch_size,),
             device="cuda",
         ).long()
-        output = self.net(batch, timesteps, return_dict=False)[0]
-        return output
+        y_hat = self.net(x, timesteps, return_dict=False)[0]
+        # some transforms need the input to be in [0,1] 
+        y_hat = torch.sigmoid(y_hat)
 
-    def global_step(self, batch: List, timestep, train: bool = False):
-        x, y = batch[0], batch[1]
-
-        x = x.to('cpu')
-        y = y.to('cpu')
-        x = self.to_starting_point(x)
-        x, y = self.to_learning_space(x, y)
-        x = x.to('cuda')
-        y = y.to('cuda')
-
-        # reshape input to the format that the huggingface lib expects
-        x = x.flatten(1).float()
-        y = y.flatten(1).float()
-
-        x.unsqueeze_(1)  # add channel dimension
-        y_hat = self.forward_step(x, timestep)
         x = x.squeeze(1)  # remove channel dimension
 
-        loss = F.mse_loss(y_hat, y)
+        loss = torch.nn.functional.mse_loss(y_hat, y)
 
-        print(loss)
+        self.log("train_loss", loss)
 
-        self.log_value(loss, "loss", train=train, prog_bar=True)
+        # reshape back into a 2D matrix
+        x = x.reshape(batch_size, -1, second_dim)
+        y_hat = y_hat.reshape(batch_size, -1, second_dim)
+        y = y.reshape(batch_size, -1, second_dim)
+        return x, y, y_hat, loss
 
-        return loss
-
-    def training_step(self, train_batch: List, batch_idx: int):
-        return self.global_step(train_batch, timestep, train=True)
-
-    def validation_step(self, val_batch: List, batch_idx: int):
-        return self.global_step(val_batch, timestep)
+    def training_step(self, batch, batch_idx):
+        _, _, _, loss = self.prepare_call_loss(batch)
+        self.log("train_loss", loss)
+        if batch_idx != 0 and batch_idx % eval_freq == 0:
+            quick_eval(batch_size, self.prepare_call_loss, test_dataloader, self)
+        return {"loss": loss}
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
@@ -113,72 +150,12 @@ class LightningDDMP(pl.LightningModule):
         )
         return [optimizer], [lr_scheduler]
 
+################  TRAINING ################
 
-
-
-################    TRAINING    ################
 if __name__ == "__main__":
-    batch_size = 1
-    # training params
-    loader = Loader()
-    train_dataloader = loader.get_train_loader(batch_size)
-    print(type(train_dataloader))
-    test_dataloader = loader.get_test_loader(batch_size)
-    print(type(test_dataloader))
-
-    ######## TRAINING PARAMS ########
-    prediction_type = "sample"  # `epsilon` (predicts the noise of the diffusion process) or `sample` (directly predicts the noisy sample` 
-    max_epochs = 10
-    channels = config["stft_num_channels"]
-    timestep = 1  # FIX TO A CONSTANT VALUE IN ORDER TO DEACTIVATE DIFFUSION AND ONLY TRAIN A U-NET
-    unet_params = {
-        "sample_size":timestep,
-        "in_channels":1,  # if we were working on rgb ims this would have to be 3 
-        "out_channels":1,
-        "block_out_channels":  (32, 64, 128),
-        "down_block_types": ('DownBlock1D', 'DownBlock1D', 'AttnDownBlock1D'),
-        "up_block_types": ('AttnUpBlock1D', 'UpBlock1D', 'UpBlock1D'),
-        "mid_block_type": 'UNetMidBlock1D',
-        "out_block_type": "OutConv1DBlock",
-        "extra_in_channels" : 0,
-        "act_fn": "swish",  # if I don't pick an act fn I get error ...
-    }
-    scheduler_params = {
-        "beta_schedule": "linear",
-        "beta_start": 0.0001,
-        "beta_end": 0.02,
-        "num_train_timesteps": 1000,
-        "prediction_type": prediction_type,
-        "clip_sample": False,
-    }
-    optimizer_params = {
-        "learning_rate": 1e-4,
-        "lr_warmup_steps": 500,
-        "n_train": len(train_dataloader) // batch_size,
-        "seed": 0,
-        "batch_size": batch_size,
-        "max_epochs": max_epochs,
-    }
-    trainer_params = {
-        # trainer parameters
-        "accumulate_grad_batches": 1,
-        "gradient_clip_val": 1,
-        "precision": "32-true",  
-        # Double precision (64, '64' or '64-true'), full precision (32, '32' or '32-true'),
-        # 16bit mixed precision (16, '16', '16-mixed') or bfloat16 mixed precision ('bf16', 'bf16-mixed').
-        # Can be used on CPU, GPU, TPUs, HPUs or IPUs.
-        "max_epochs": max_epochs,
-        "accelerator": "auto",
-        "devices": "auto",
-        "num_nodes": 1}
-    
-    ######## INVOKE TRAINING ########
-    net = UNet1DModel(**unet_params)
     scheduler = DDPMScheduler(**scheduler_params)
-    pipeline = DDPMPipeline1DCond(net, scheduler)
+    net = UNet1DModel(**unet_params)
     model = LightningDDMP(net, scheduler, prediction_type=prediction_type, optimizer_params=optimizer_params)
-    # wandb_logger = WandbLogger(project='generative inversion')
-    # wandb_logger.experiment.batch_size = batch_size
-    trainer = pl.Trainer(**trainer_params)
-    # trainer = pl.Trainer(**trainer_params, logger=wandb_logger)
-    trainer.fit(model, train_dataloaders=train_dataloader)
+    wandb_logger = WandbLogger(project='generative inversion')
+    trainer = pl.Trainer(max_epochs=max_epochs, logger=wandb_logger)
+    trainer.fit(model=model, train_dataloaders=train_dataloader)
